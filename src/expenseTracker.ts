1e51c42e3900fd76311fa6b28778719b3d7d2738
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-05-12 20:48:35 +0200

    feat: Add basic function structure and basic typing

--- src/expenseTracker.ts
+++ src/expenseTracker.ts
@@ -1,3 +1,34 @@
 const generateExpenseReport = (dailyExpenses: number[], dailyBudget: number) => {
+    interface  ExpenseReport{
+        travelDays: number,             //dies totals del viatge
+        expenseDays: number,            //dies amb despesa >0
+        dailyBudget: number,            //pressupost rebut
+        averageDailyExpense: number,    //mitjana de despesa diària
+        underBudget: boolean,           //true si mitjana <= pressupost
+        rating: 1 | 2 | 3,              //valoració (1, 2 o 3)
+        feedback: string                //missatge descriptiu        
+    }
     
+    const expenseReport : ExpenseReport {
+        travelDays: (dailyExpenses.length),
+        expenseDays: calculateExpenseDays   dailyExpenses.filter(num => num > 0),
+        dailyBudget: dailyBudget,
+        averageDailyExpense: dailyExpenses.reduce((acumulator, num) => acumulator + num, 0) / dailyExpenses.length,
+        underBudget: (dailyExpenses <= dailyBudget),
+        rating: if(averageDailyExpense <= dailyBudget){
+                return 1
+            }else if(averageDailyExpense <= dailyBudget * 1.2){
+                return 2
+            }
+            return 3,              //valoració (1, 2 o 3)
+        feedback: if(rating === 1){
+            return "Excel·lent gestió!"
+        }else if(rating === 2){
+            return "Correcte, però ajustat" 
+        }
+        return "Pot millorar"
+    }
+
 }
+
+console.log(generateExpenseReport([50, 0, 120, 85], 100));
